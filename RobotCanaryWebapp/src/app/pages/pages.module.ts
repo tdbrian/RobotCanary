1cfdd3c25d787507acf217c23c59c3c93c5d06fe
Thomas Brian <tbrian@appriver.com>
2018-06-30 12:59:53 -0500

    Fix some route outliers

--- RobotCanaryWebapp/src/app/pages/pages.module.ts
+++ RobotCanaryWebapp/src/app/pages/pages.module.ts
@@ -4,16 +4,6 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { RouterModule, Routes } from '@angular/router';
 import { MissingPageComponent } from './missing-page/missing-page.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { SaveAppComponent } from './apps/save-app/save-app.component';
-import { ListAppsComponent } from './apps/list-apps/list-apps.component';
-import { EndpointsComponent } from './endpoints/endpoints.component';
-import { EntitiesComponent } from './entities/entities.component';
-import { ApplicationsComponent } from './applications/applications.component';
-import { UserInterfacesComponent } from './user-interfaces/user-interfaces.component';
-import { AppsListComponent } from './user-interfaces/apps/apps-list/apps-list.component';
-import { ExternalServicesComponent } from './integrations/external-services/external-services.component';
-import { ExternalDataComponent } from './integrations/external-data/external-data.component';
 
 const routes: Routes = [];
 
@@ -21,9 +11,8 @@ const routes: Routes = [];
   imports: [
     CommonModule,
     SharedModule,
-    RouterModule.forChild(routes),
-    DashboardModule
+    RouterModule.forChild(routes)
   ],
-  declarations: [MissingPageComponent, SaveAppComponent, ListAppsComponent, EndpointsComponent, EntitiesComponent, ApplicationsComponent, UserInterfacesComponent, AppsListComponent, ExternalServicesComponent, ExternalDataComponent]
+  declarations: []
 })
 export class PagesModule { }
--- RobotCanaryWebapp/src/app/pages/user-interfaces/user-interfaces.module.ts
+++ RobotCanaryWebapp/src/app/pages/user-interfaces/user-interfaces.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UiComponentsComponent } from './apps/ui-components/ui-components.component';
-import { ThemesComponent } from './apps/themes/themes.component';
-import { SystemActionsComponent } from './state/system-actions/system-actions.component';
+import { UiComponentsComponent } from './ui-components/ui-components.component';
+import { ThemesComponent } from './themes/themes.component';
+import { SystemActionsComponent } from '../state/system-actions/system-actions.component';
 
 @NgModule({
   imports: [
